0e8ec845a91f5e5597c37d1c8712a03f111d4927
iamrazzakk <rakibt23p@gmail.com>
2025-10-17 13:09:12 +0600

    solve problem 1

---
+++ assignment2Problem1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//  ____    _    _______ _______      _  __
+// |  _ \  / \  |__  /_ _|__  (_)_ __| |/ /
+// | |_) |/ _ \   / / | |  / /| | '__| ' /
+// |  _ </ ___ | / /_ | | / /_| | |  | . \
+// |_| \_/_/  |_|/____|___/____|_|_|  |_|\_\
+
+// Problem Link => https://www.hackerrank.com/contests/assignment-02-a-introduction-to-c-programming-a-batch-08/challenges/say-it
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        /* code */
+        // 1. I Want More Assignments
+        //  2. I Want More Assignments
+        //  3. I Want More Assignments
+        //  4. I Want More Assignments
+        //  5. I Want More Assignments
+        printf("%d. I Want More Assignments\n", i);
+    }
+
+    return 0;
+}
