b3d279643b255ed21a10a69cb6b0f05fffe2f7c6
iamrazzakk <rakibt23p@gmail.com>
2025-10-19 20:21:56 +0600

    swap the value

---
+++ removeFromArray.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        scanf("%d", &arr[i]);
+    }
+    int idx;
+    scanf("%d", &idx);
+    for (int i = idx; i < n -1; i++)
+    {
+        /* code */
+        arr[i] = arr[i + 1];
+    }
+    
+    for (int i = 0; i < n -1; i++)
+    {
+        /* code */
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
