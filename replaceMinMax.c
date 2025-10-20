59267e4638e825d0301f4ba1dc7d100d480cad9e
iamrazzakk <rakibt23p@gmail.com>
2025-10-20 23:37:36 +0600

    count from string

---
+++ replaceMinMax.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        scanf("%d", &arr[i]);
+    }
+    int i = 0;
+    int j = n -1;
+    while (i < j)
+    {
+        /* code */
+        int tem = arr[i];
+        arr[i]=  arr [j];
+        arr[j] = tem;
+
+    }
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        printf("%d", arr[i]);
+    }
+    
+    return 0;
+}
