518a3370e3444aad5020128480aa42b6c3c7fa08
iamrazzakk <rakibt23p@gmail.com>
2025-10-22 19:47:57 +0600

    compare with carecter

---
+++ C_Compare.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{
+    char a[101], b[101];
+    scanf("%s %s", a, b);
+    int i = 0;
+    while (1)
+    {
+        if (a[i] == "\0" && b[i] == "\0")
+        {
+            printf("%s\n", a);
+            break;
+            /* code */
+        }
+        else if (a[i] == "\0")
+        {
+            /* code */
+            printf("%s\n", a);
+
+            break;
+        }
+        else if (b[i] == "\0")
+        {
+            /* code */
+            printf("%s\n", b);
+
+        }
+        else if (a[i] < b[i])
+        {
+            printf("%s\n", a);
+            break;
+        }
+        else if (a[i] > b[i])
+        {
+            printf("%s\n", b);
+            break;
+        }
+        else
+        {
+            i++;
+        }
+
+        /* code */
+    }
+
+    return 0;
+}
