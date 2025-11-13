185cc4373dc1d2a71442694fd95b7828b53bcf30
iamrazzakk <rakibt23p@gmail.com>
2025-11-13 12:59:00 +0600

    resolve print_digits_using_recursion problem

--- D_Print_Digits_using_Recursion.c
+++ D_Print_Digits_using_Recursion.c
@@ -7,8 +7,33 @@
 
 // Problem Link => https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/D
 
+void recursionFunction(int n)
+{
+    if (n == 0)
+    {
+        /* code */
+        return;
+    }
+    recursionFunction(n / 10);
+    int last = n % 10;
+    printf("%d ", last);
+}
+
 int main()
 {
-    
-    return 0;
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        if (n == 0)
+        {
+            /* code */
+            printf("0");
+        }
+
+        recursionFunction(n);
+        printf("\n");
+    }
 }
